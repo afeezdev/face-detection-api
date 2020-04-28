7a28b43ee3b5d2325eb9f69be125bf3ada398dd8
afeezdev <afeezdev@gmail.com>
2020-04-28 23:05:05 +0100

    change the postgres connection  to elephantsql.com

--- server.js
+++ server.js
@@ -10,29 +10,68 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      connectString : process.env.DATABASE_URL,
-      ssl: true,
-    }
-  });
+  client: "pg",
+  connection: {
+    host: "drona.db.elephantsql.com",
+    user: "gshlakiw",
+    password: "94zn8iiDQ4AWnEgQcYy8YiZJTcTuxYk7",
+    database: "gshlakiw",
+  },
+});
 
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "apples",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+ 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("it is working");
+});
 
-app.get('/', (req, res) =>{ res.send('it is working') });
+app.post("/signin",(req, res) => {
+  signin.handleSignin(req, res, db, bcrypt);
+})
 
-app.post('/signin', signin.handleSignin(db, bcrypt));
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
-app.put('/image' , (req, res) => { image.handleImage(req, res, db) });
-app.post('/imageUrl' , (req, res) => { image.handleApiCall(req, res) });
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageUrl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(process.env.PORT || 3000, () =>{
-    console.log(`app is running on port 3000 ${process.env.PORT}`) 
+    console.log(`app is running on port 3000  ${process.env.PORT}`) 
 })
 
 
